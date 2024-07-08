e9ff95783431a493d1a70adf20dd402c5eccc943
thepralad <prgurung04@gmail.com>
2024-07-08 17:10:10 +0530

    chore: Refactor todo-list.js to use pushItem and popItem functions

--- src/scripts/todo-list.js
+++ src/scripts/todo-list.js
@@ -1,20 +1,28 @@
-const Projects = {
-    today: [
-        {
-            title: "Do the dishes",
-            priority: "high"
-        }, 
-        {
-            title: "Wash a car",
-            priority: "mid"
-        }
-    ]
+import { pushToLocalStorage } from "./todo-storage";
+import { Log } from "./utility";
+const Projects = {}
+
+const pushItem = function(value){
+
+    //This checks if a project(array), exists or not.
+    if (!Projects.hasOwnProperty(value.project)) {
+        Projects[value.project] = [];
+    }
+
+    Projects[value.project].push(value);
+    pushToLocalStorage(Projects);
+    Log(value.title + " added!", "green");
+
 }
 
-const addProject = function(value){
-    
+const popItem = function(value){
+    Projects[value.project].splice(Projects[value.project].indexOf(value), 1);
+    pushToLocalStorage(Projects);
+    Log(value.title + " removed!", "red");
 }
 
-const checkProjInProjects = function(value){
-    
-}       
+export {
+    pushItem,
+    popItem
+}
+     
