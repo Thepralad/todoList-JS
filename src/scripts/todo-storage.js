570d86da1690a784ab60267de7a3666817daa3f4
thepralad <prgurung04@gmail.com>
2024-07-08 17:16:47 +0530

    feat: module to retrieve data from thelocalStorage

--- src/scripts/todo-storage.js
+++ src/scripts/todo-storage.js
@@ -1,7 +1,11 @@
 function pushToLocalStorage(project){
     localStorage.setItem('Projects', JSON.stringify(project))
 }
+function retrieveFromLocalStorage(){
+    const data = JSON.parse(localStorage.getItem('Projects'));
+    return data;
+}
 
 export {
-    pushToLocalStorage
+    pushToLocalStorage, retrieveFromLocalStorage
 }
