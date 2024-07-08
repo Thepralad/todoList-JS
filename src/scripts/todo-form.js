48eaef01c11d130335080aa0309728971c0f0f0a
thepralad <prgurung04@gmail.com>
2024-07-08 18:22:41 +0530

    feat: implemented input form

---
+++ src/scripts/todo-form.js
@@ -0,0 +1,30 @@
+import { pushItem } from "./todo-list";
+import { Items } from "./todo-items";
+let CURRENT_PROJ_STATE = 'today';
+
+function projectBtnSelection(){
+    const projectBtns = document.getElementsByClassName('project');
+    for (const btn of projectBtns) {
+        btn.addEventListener('click', e => {
+            CURRENT_PROJ_STATE = e.target.textContent;
+        })
+    }
+}
+
+function inputForm(){
+   document.getElementById('submit').addEventListener('click', e => {
+    const title = document.getElementById('title').value;
+    const date = document.getElementById('date').value;
+    const priority = document.getElementById('priority').value;
+    const project = CURRENT_PROJ_STATE;
+
+    const newItem = new Items(title, date, priority, project)
+    pushItem(newItem);
+    e.preventDefault();
+   })
+
+
+}
+export{
+    inputForm, projectBtnSelection
+}
