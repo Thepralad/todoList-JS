9e16d850bc8c44e9c16fe9acf8fcc6d2908da725
thepralad <prgurung04@gmail.com>
2024-07-08 16:20:14 +0530

    feat: Add todo list functionality to manage tasks

---
+++ src/scripts/todo-list.js
@@ -0,0 +1,20 @@
+const Projects = {
+    today: [
+        {
+            title: "Do the dishes",
+            priority: "high"
+        }, 
+        {
+            title: "Wash a car",
+            priority: "mid"
+        }
+    ]
+}
+
+const addProject = function(value){
+    
+}
+
+const checkProjInProjects = function(value){
+    
+}       
