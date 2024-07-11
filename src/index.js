319b78be3f863d213b8277de21dcd582fad05ffc
thepralad <prgurung04@gmail.com>
2024-07-11 14:05:45 +0530

    chore: making saperate modules for different task

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { Items } from "./scripts/todo-items";
 import { pushItem, popItem } from "./scripts/todo-list";
+import {inputForm, projectBtnSelection} from "./scripts/todo-form";
 
-
-
+projectBtnSelection();
+inputForm();
 
 
 
---
+++ src/scripts/todo-dom.js
@@ -0,0 +1,47 @@
+import { retrieveFromLocalStorage } from "./todo-storage";
+import { CURRENT_PROJ_STATE } from "./todo-form.js";
+
+function Data(){
+    const data = retrieveFromLocalStorage();
+    const currentProj = data[CURRENT_PROJ_STATE];
+    return currentProj;
+}
+
+function isCurrentProjectValid(){
+    const currentProj = Data();
+    if(Array.isArray(currentProj)) return true;
+    else  return false;
+}
+
+function displayProject(){
+    const currentProj = Data();
+    const _itemsDiv = document.getElementById('items');
+
+    _itemsDiv.textContent = '';
+
+    if (isCurrentProjectValid()) {
+        for (const item of currentProj) {
+            const itemDiv = document.createElement('div');
+            const titleH4 = document.createElement('h4');
+            const dateP = document.createElement('p');
+            const priorityP = document.createElement('p');
+
+            titleH4.textContent = item.title;
+            dateP.textContent = item.date;
+            priorityP.textContent = item.priority;
+
+            itemDiv.appendChild(titleH4);
+            itemDiv.appendChild(dateP);
+            itemDiv.appendChild(priorityP);
+
+            _itemsDiv.appendChild(itemDiv);
+        }
+    } else {
+        console.error('currentProj is not an array:', currentProj);
+    }
+    
+}
+
+export{
+    displayProject, Data
+}
