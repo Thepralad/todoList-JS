d30a9a9d98a3cb764ad1b53c01ba07bc8b5f32d2
thepralad <prgurung04@gmail.com>
2024-07-13 15:58:48 +0530

    feat: Refactor todo-list.js to use ProjectTemplate for initializing projects

--- src/scripts/todo-list.js
+++ src/scripts/todo-list.js
@@ -1,25 +1,25 @@
 import { pushToLocalStorage } from "./todo-storage";
 import { Log } from "./utility";
 import { displayProject } from "./todo-dom";
-import { projectBtnSelection } from "./todo-form";
 
+const ProjectTemplate = {
+    today: []
+}
 function loadProjectFromLocalStorage(){
     const storedProjects = JSON.parse(localStorage.getItem('Projects'))
     if(storedProjects){
         return storedProjects;
     }
     else{
-        const Projects = {
-            today: []
-        }
-        return Projects;
+        return ProjectTemplate;
     }
 }
 
 
+
 const pushItem = function(value){
     const Projects = loadProjectFromLocalStorage();
-    //This checks if a project(array), exists or not.
+
     if (!Projects.hasOwnProperty(value.project)) {
         Projects[value.project] = [];
     }
@@ -39,6 +39,7 @@ const popItem = function(value){
 
 export {
     pushItem,
-    popItem
+    popItem,
+    ProjectTemplate,
 }
      
