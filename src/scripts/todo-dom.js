e26fbb518f4dbad3f5bea1616e0e20f5975e5189
thepralad <prgurung04@gmail.com>
2024-07-13 15:58:28 +0530

    refactor: Improve displayProject function to include checkboxes and horizontal lines

--- src/scripts/todo-dom.js
+++ src/scripts/todo-dom.js
@@ -18,23 +18,30 @@ function displayProject(){
     const _itemsDiv = document.getElementById('items');
 
     _itemsDiv.textContent = '';
-
     if (isCurrentProjectValid()) {
         for (const item of currentProj) {
+
+            const checkBox = document.createElement('input');
+            checkBox.setAttribute('type', 'checkbox');
+            
             const itemDiv = document.createElement('div');
             const titleH4 = document.createElement('h4');
             const dateP = document.createElement('p');
             const priorityP = document.createElement('p');
+            const horizontalLine = document.createElement('hr');
 
             titleH4.textContent = item.title;
             dateP.textContent = item.date;
             priorityP.textContent = item.priority;
 
+            itemDiv.appendChild(checkBox);
             itemDiv.appendChild(titleH4);
             itemDiv.appendChild(dateP);
             itemDiv.appendChild(priorityP);
+            itemDiv.appendChild(horizontalLine);
 
             _itemsDiv.appendChild(itemDiv);
+
         }
     } else {
         console.error('currentProj is not an array:', currentProj);
@@ -43,5 +50,5 @@ function displayProject(){
 }
 
 export{
-    displayProject, Data
+    displayProject
 }
