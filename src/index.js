b5ca854fc11f6cea56241d40c412a855556b7764
thepralad <prgurung04@gmail.com>
2024-07-13 16:00:32 +0530

    added some functions call

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import { Items } from "./scripts/todo-items";
-import { pushItem, popItem } from "./scripts/todo-list";
-import {inputForm, projectBtnSelection} from "./scripts/todo-form";
+import {inputForm, projectBtnSelection, createProject} from "./scripts/todo-form";
+import { displayProject } from "./scripts/todo-dom";
 
 projectBtnSelection();
 inputForm();
-
+displayProject();
 
 
