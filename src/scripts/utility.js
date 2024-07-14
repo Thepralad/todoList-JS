847315bc7450cbee0c4ba6f1b1717de03e5b5043
thepralad <prgurung04@gmail.com>
2024-07-14 22:20:10 +0530

    feat: Added a function to check dublicates

--- src/scripts/utility.js
+++ src/scripts/utility.js
@@ -1,7 +1,23 @@
+import { retrieveFromLocalStorage } from "./todo-storage"
+
 const Log = function(message, color){
     console.log(`%c${message}`, `color: ${color}`)
 }
 
+const checkDublicates = function(value, project){
+    const data = retrieveFromLocalStorage();
+    const arr = data[project];
+        
+    for (const item of arr) {
+        if(item.title === value){
+                return false;
+            }
+        }
+    return true;
+    }
+
+
 export{
-    Log
+    Log,
+    checkDublicates
 }
