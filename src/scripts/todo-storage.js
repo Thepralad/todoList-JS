54905f95a981d590f9e0fb333262e34de453a2f6
thepralad <prgurung04@gmail.com>
2024-07-14 22:22:23 +0530

    refactor: Update retrieveFromLocalStorage function to handle missing data

--- src/scripts/todo-storage.js
+++ src/scripts/todo-storage.js
@@ -1,6 +1,9 @@
 function pushToLocalStorage(project){
     localStorage.setItem('Projects', JSON.stringify(project))
 }
+
+//This function returns the Projects from the local storage if available,
+//If not, it will return an object with an array['today'] in it 
 function retrieveFromLocalStorage(){
     const data = JSON.parse(localStorage.getItem('Projects'));
     if(data === null){
